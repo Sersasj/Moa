5ee8502656a9c7b83418711d4d7ed129764cfca8
Sersasj <sasjsergioalvarezjunior@gmail.com>
2022-03-19 22:57:41 -0300

    começando nearest neighbor

--- Trab.py
+++ Trab.py
@@ -32,24 +32,51 @@ def remove_six(text):
         
     return text[count_qnt:len(text)-3]
 
+# Cria lista com os Vertices
 def populate(text):
     list_vertice = []
     text = text.split('\n')
     text.remove("")
     for element in text:
         element = element.split(" ")
-        element = element[1:3]  
+        element = element[1:]  
         list_vertice.append(Vertice(element[0], element[1]))
     return list_vertice
-            
-        
-    
 
+# Inicializa vertices como não visitado
+# Nao sei se pode iniciar com um visitado/ nao visitado na classe
+# coloquei aqui como : [[Vertice,visitado],...]            
+def init_false(list_vertice):
+    list_aux = []
+    for vertice in list_vertice:
+        list_aux.append([vertice,False])
+    return list_aux    
+
+# percorre os não percorridos, acha o menor
+def find_nearest(visited_list):
+    
+    
+    print("a")                
+    
+# Metodo construtivo 
+# Neareast Neighbor
+def nearest(list_vertice):
+    path = []
+    
+    visited_list = init_false(list_vertice)
+    # Inicia no primeiro vertice
+    visited_list[0][1] = True
+    path = [0]
+    print(visited_list)
+    #while len(path) < len(list_vertice):
+       # print("a")
+    
 
 def main():
     content = read_input()
     content = remove_six(content)
     list_vertice = populate(content)
-    print(list_vertice)
+    nearest(list_vertice)
+    #print(list_vertice)
 if __name__ == '__main__':
    main()
