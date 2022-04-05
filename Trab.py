aece5b41874464eb33bf0653952bf508f88a5808
Sersasj <sasjsergioalvarezjunior@gmail.com>
2022-04-04 21:02:53 -0300

    versão aceita run.codes

--- Trab.py
+++ Trab.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+# Sergio Alvarez
+# Guilherme Ferrari
 from sys import stdin
 
 import sys
 import math
+import time
+end = 0
 class Vertice:
     def __init__(self, x, y):
         self.x = x
@@ -17,32 +21,23 @@ def read_input():
       break
   return input_str[6:len(input_str)-1]
 
-"""
-def read_input():
-  with open(sys.argv[1], 'r') as f:
-    for i, line in enumerate(f):
-      if i == 5:          
-        content = f.read()
-        break;
-        
-  return content[:len(content)-4]
-"""
+
 
 # Cria lista com os Vertices
 def populate(text):
-    list_vertice = []
+    vertex_list = []
     for element in text:
-        element = element.split(" ")
+        element = element.split()
         element = element[1:]  
-        list_vertice.append(Vertice(int(element[0]), int(element[1])))
-    return list_vertice
+        vertex_list.append(Vertice(int(element[0]), int(element[1])))
+    return vertex_list
 
 # Inicializa vertices como não visitado
 # Nao sei se pode iniciar com um visitado/ nao visitado na classe
 # coloquei aqui como : [[Vertice,visitado],...]            
-def init_false(list_vertice):
+def init_false(vertex_list):
     list_aux = []
-    for vertice in list_vertice:
+    for vertice in vertex_list:
         list_aux.append([vertice,False])
     return list_aux    
 
@@ -74,29 +69,69 @@ def find_nearest(visited_list, vertice_index):
 
 # Metodo construtivo 
 # Neareast Neighbor
-def nearest(list_vertice):
+def nearest(vertex_list):
     path = [] 
     path_w = []
     
-    visited_list = init_false(list_vertice)
+    visited_list = init_false(vertex_list)
     # Inicia no primeiro vertice
     visited_list[0][1] = True
     path = [0]
-    for i in range(len(list_vertice)-1):
+    for i in range(len(vertex_list)-1):
         near, w = find_nearest(visited_list, path[i])
         path.append(near)
         path_w.append(w)
         visited_list[near][1] = True
     #Volta pro inicio
-    path_w.append(dist(list_vertice[path[0]].x, list_vertice[path[0]].y, list_vertice[path[len(path)-1]].x, list_vertice[path[len(path)-1]].y))
+    path_w.append(dist(vertex_list[path[0]].x, vertex_list[path[0]].y, vertex_list[path[len(path)-1]].x, vertex_list[path[len(path)-1]].y))
     path.append(path[0])
 
-    print(sum(path_w))
-    
+    return path
+
+def cost(path, vertex_list):
+  path_w = 0
+  for i in range(len(path)-1):    
+    path_w += dist(vertex_list[path[i]].x,vertex_list[path[i]].y,
+                   vertex_list[path[i+1]].x,vertex_list[path[i+1]].y)
+  return path_w
+
+def two_opt(path,vertex_list):
+  start = time.time()
+  best = path.copy()
+  improved = True
+  while improved:
+    improved = False
+    for i in range(1, len(path)-2):
+      for j in range(i+1, len(path)):
+        if j-i == 1:
+          continue
+        new_path = path[:]
+  
+        new_path[i:j] = path[j-1:i-1:-1]
+        if(cost(new_path, vertex_list) < cost(path, vertex_list)):
+          end = time.time()
+          #print(end - start)
+          #print("antigo ", cost(path, vertex_list), "novo ",cost(new_path, vertex_list))
+          best = new_path
+          if (end - start > 5):
+            return best
+          improved = True
+          path = best.copy()
+  return best  
+
 
 def main():
+    start = time.time()
+
     content = read_input()
-    list_vertice = populate(content)
-    nearest(list_vertice)
+    vertex_list = populate(content)
+    
+    path = nearest(vertex_list)
+    path2 = two_opt(path, vertex_list)
+    end = time.time()
+    #print(end - start)
+    print("aa", cost(path, vertex_list))
+  
+    print("bb", cost(path2, vertex_list))
 if __name__ == '__main__':
    main()
